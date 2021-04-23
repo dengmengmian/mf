0bb57921b287dd4067e54d32baafa9a4418e9c87
dengmengmian(麻凡) <my@dengmengmian.com>
2021-04-23 15:19:23 +0800

    修改版本信息

--- cmd/version.go
+++ cmd/version.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+Copyright © 2021 NAME HERE <my@dengmengmian.com>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -24,28 +24,16 @@ import (
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "版本信息",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("v0.0.1")
+		version()
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(versionCmd)
+}
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// versionCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// versionCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+func version() {
+	fmt.Println("v0.0.1")
 }
